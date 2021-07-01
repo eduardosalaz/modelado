2d3384de108057eb2fd559c4ef5115ab632d5a58
eduardosalaz <eduardosalaz@outlook.com>
2021-07-01 17:11:27 -0500

    añadir distribuciones

---
+++ distribuciones.jl
@@ -0,0 +1,50 @@
+include("generador.jl")
+using Statistics, Distributions
+function normal()
+    generados = linealCongruencial(600, 7919, 19, 401, 5)
+    normal = []
+    μ = mean(generados)
+    σ = std(generados)
+    inicio = 1
+    fin = 12
+    for i in 1:50
+        sample = generados[inicio:fin]
+        suma = sum(sample)
+        x = μ  + σ*(suma-6)
+        norm = x
+        println(norm)
+        push!(normal, norm)
+        inicio += 12
+        fin +=12
+    end
+    return normal
+end
+
+function uniforme(a, b)
+    println("----------UNIFORME----------")
+    generados = linealCongruencial(100, 7919, 19, 401, 5)
+    uniforme = []
+    for R ∈ generados
+        x = a + R * (b-a)
+        unif = x
+        println(unif)
+        push!(uniforme, unif)
+    end
+    return uniforme
+end
+
+function exponencial(λ)
+    println("----------EXPONENCIAL----------")
+    generados = linealCongruencial(100, 7919, 19, 401, 5)
+    exponencial = []
+    for R ∈ generados
+        expo = -log(R)/λ
+        println(expo)
+        push!(exponencial, expo)
+    end
+    return exponencial
+end
+
+normal()
+uniforme(0, 10)
+exponencial(1.5)
--- generador.jl
+++ generador.jl
@@ -1,11 +1,19 @@
+function linealCongruencial(n, m, a, c, x)
+    resultados = Float64[]
+    for i ∈ 1:n
+        r = (a * x + c) % m
+        x = r
+        r = r / m #normalizar
+        push!(resultados, r)
+        #println("Numero $i: $r")
+    end
+    return resultados
+end
 n = 100
 m = 7919
 a = 19
 c = 401
 x = 5
-for i in 1:n
-    r = (a * x + c) % m
-    global x = r
-    #r = r / m #normalizar
-    println("Numero $i: $r")
-end
+# linealCongruencial(n, m, a, c, x)
+
+# Analizar las presentaciones adjuntas y hacer un programa 
